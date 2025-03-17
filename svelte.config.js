a49e8e23b3aad280ba851ccfd2ee1b4e1fa13b24
Fabio Telloli <fabio.telloli@istitutoagnelli.it>
2025-03-17 13:36:00 +0100

    terzo commit

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
             strict: true
         }),
         paths: {
-			base: process.argv.includes('dev') ? '' : '/mysveltekit'
+			base: process.argv.includes('dev') ? '' : '/myTodosWeb'
 		}
 	}
 };
